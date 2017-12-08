a7e385288e55de758d93185678dd02d97d12cc6a
prabhu <ppprabhu2010@gmail.com>
2017-12-08 17:28:53 +0530

    flips using rssfeed and Jquery
    This is the 3 page flip using Jquery

---
+++ js/flipInits.js
@@ -0,0 +1,37 @@
+var Page = (function() {
+				
+				var config = {
+						$bookBlock : $( '#block' ) 
+					},
+					init = function() {
+						config.$bookBlock.bookblock( {
+							speed : 1000,
+							shadowSides : 0.8,
+							shadowFlip : 0.4
+						} );
+						initEvents();
+					},
+					initEvents = function() {
+						
+						var $slides = config.$bookBlock.children();
+
+						 
+						
+						// add swipe events
+						$slides.on( {
+							'swipeleft' : function( event ) {
+								config.$bookBlock.bookblock( 'next' );
+								return false;
+							},
+							'swiperight' : function( event ) {
+								config.$bookBlock.bookblock( 'prev' );
+								return false;
+							}
+						} );
+					};
+
+					return { init : init };
+
+			})();
+		 
+				Page.init();
